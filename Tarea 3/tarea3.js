b6c65baf407257506c213936179ed18e0ecd5b30
RaulAlanGarcia <raul.garcia@diginteligencia.com>
2017-05-11 15:20:23 -0500

    Tarea 3
    Canvas

---
+++ Tarea 3/tarea3.js
@@ -0,0 +1,60 @@
+
+
+triangle({x: 200, y: 40, color: 'blue', size: 60})
+
+var rect1 = { x: 200, y: 100, width: 30, height: 50, color: 'blue'}
+
+rect(rect1)
+
+
+
+
+
+
+Diamond({x: 50, y: 150, color: 'yellow', size: 20})
+triangle({x: 50, y: 150, color: 'yellow', size: 20})
+
+
+for (var j = 0; j < 50; j++) {
+
+  for (var i = 0; i < 200; i++) {
+
+    var color2 = '#'+ 1 * i  + '0000'
+  Diamond({x: 50 + (i * 10) , y: 170 + j , color: color2, size: 30})
+
+                                 }
+
+                               }
+
+
+
+ for (var j = 0; j < 550; j++) {
+
+   for (var i = 0; i < 200; i++) {
+
+     var color3 = '#0000'+ 1 * i
+   Diamond({x: -230 + (i * 10) , y: 480 + j , color: color3, size: 30})
+
+                                  }
+
+                                }
+
+
+
+                      monster()
+
+                      var gradienteAzul = context.createRadialGradient(45,4,1,90,65,410);
+                      gradienteAzul.addColorStop(0,"white");
+                      gradienteAzul.addColorStop(1,"blue");
+
+
+
+
+    circle({x: 2, y: 650, color: gradienteAzul , size: 420})
+
+    circle({x: 1, y: 750, color: gradienteAzul , size: 320})
+
+   triangle({x: 10, y: 950,width:2, color:"#000022", size: 1200})
+
+
+  Diamond({x: -2000, y: 550,width:2, color:"#000022", size: 3200})
